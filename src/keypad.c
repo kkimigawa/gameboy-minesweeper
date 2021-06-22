44b3693f8e48ceecc26498d9d5989bcbb4d6eb2b
kkimigawa <kkimigawa@gmail.com>
2021-06-23 01:47:43 +0900

    Refactoring keypad

--- src/keypad.c
+++ src/keypad.c
@@ -1,51 +1,51 @@
 #include "keypad.h"
 
-static uint8_t current_key;
-static uint8_t prev_key;
-static uint8_t repeat_count;
+static uint8_t _current_key;
+static uint8_t _prev_key;
+static uint8_t _repeat_count;
 
 void keypad_init()
 {
-    current_key = 0;
-    prev_key = 0;
-    repeat_count = 0;
+    _current_key = 0;
+    _prev_key = 0;
+    _repeat_count = 0;
 }
 
 void keypad_update()
 {
-    prev_key = current_key;
-    current_key = joypad();
+    _prev_key = _current_key;
+    _current_key = joypad();
 }
 
 int8_t keypad_trigger(enum KeypadKey key)
 {
-    return current_key & key && !(prev_key & key);
+    return _current_key & key && !(_prev_key & key);
 }
 
 int8_t keypad_down(enum KeypadKey key)
 {
-    return current_key & key;
+    return _current_key & key;
 }
 
 int8_t keypad_repeat(enum KeypadKey key, uint8_t begin, uint8_t interval)
 {
-    if ((current_key & key) == 0) {
+    if ((_current_key & key) == 0) {
         return 0;
     }
 
-    if ((prev_key & key) == 0 && current_key != prev_key) {
-        repeat_count = 0;
+    if ((_prev_key & key) == 0 && _current_key != _prev_key) {
+        _repeat_count = 0;
         return 1;
     }
 
-    repeat_count++;
+    _repeat_count++;
 
-    if (repeat_count > begin + interval) {
-        repeat_count = begin + 1;
+    if (_repeat_count > begin + interval) {
+        _repeat_count = begin + 1;
         return 1;
     }
 
-    if (repeat_count == begin) {
+    if (_repeat_count == begin) {
         return 1;
     }
 
